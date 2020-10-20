9229e7d90d0c5699e4df1a15f5288dadb231d2dc
Saba Bh <s.bahonar.bio@gmail.com>
2020-10-20 17:53:17 +0330

    n

---
+++ WSI/RNAseqAnalysis/InstallPackages.R
@@ -0,0 +1,19 @@
+if (!requireNamespace("BiocManager", quietly=TRUE))
+  install.packages("BiocManager")
+BiocManager::install("TCGAbiolinks")
+####
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("BioinformaticsFMRP/TCGAbiolinksGUI.data")
+BiocManager::install("BioinformaticsFMRP/TCGAbiolinks")
+####
+install.packages("tidyverse")
+install.packages("readr")
+#####
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("edgeR")
+#####
+install.packages("gProfiler2")
+#####
