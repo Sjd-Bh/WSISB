3a81830d753f11be93f00cbd7d2d1d7ca7122a8d
Saba Bh <s.bahonar.bio@gmail.com>
2020-11-03 16:02:08 +0330

    n

--- WSI/model/runModle.R
+++ WSI/model/runModle.R
@@ -1,42 +1,46 @@
-
-
 rm(list = ls())
+##########################################
+source("/home/montazeri/WSISB/WSI/model/requiredFunction.R")
 
 library(rhdf5)
-
 supertiles <- h5read("/mnt/montazeri/data_from_authors/TCGA_slic_100.h5","X")
-save_folder = "/home/montazeri/results/lassoModel/"
+load("/home/montazeri/results/GSVA_DA_PA/gsva.RData")
+y <- t(gsa)
 
-for (j in 1:100) {
-  x = c()
-  for (i in 1:10514) {
-    x = rbind(x, as.vector(as.matrix(supertiles[,j,i])))
-  }
-  #save(x,file = paste0(save_folder,"x",j,".RData"))
-  for (z in 1:repetition) {
-    TrainTest = TrainTestProducer(x,y)
-    save(TrainTest,file = "")
-    
-    
-  }
-}
+save_folder = "/home/montazeri/results/data/"
+saveFolder = "/home/montazeri/results/lassoModel/"
 
-######
+dir.create(save_folder, recursive = TRUE, showWarnings = FALSE)
+dir.create(saveFolder, recursive = TRUE, showWarnings = FALSE)
+
+
+repetition = 5
+numOfSamples = 10514
 
 for (i in 1:repetition) {
-  TrainTest = TrainTestProducer(x,y)
-  save(TrainTest,file = "")
-}
-####
-
-for (j in 1:100) {
-  x = c()
-  for (i in 1:10514) {
-    x = rbind(x, as.vector(as.matrix(supertiles[,j,i])))
+  
+  ### TrainTest_index
+  TrainTestIdx = TrainTestIdxProducer(numOfSamples)
+  save(TrainTestIdx,file = paste0(save_folder,"split",i,".RData"))
+  fname = paste0(saveFolder,"split",i,".RData")
+  
+  if(!file.exists(fname)){
+    ### train_test splitting and lasso model
+    supertilePred <- list()
+    for (j in 1:100){
+      x <- t(supertiles[,j,])
+      Xtrain <- x[TrainTestIdx$nTrain,]
+      Ytrain <- y[TrainTestIdx$nTrain,]
+      Xtest <- x[TrainTestIdx$nTest,]
+      modelFit <- lassoLR(Xtrain,Ytrain)
+      pred <- lassoLRpred(modelFit,Xtest)
+      supertilePred[[j]] <- pred
+    }
+    M <- do.call(cbind, supertilePred)
+    M <- array(M, dim=c(dim(supertilePred[[1]]), length(supertilePred)))
+    WSIpred <- apply(M, c(1, 2), mean, na.rm = TRUE)
+    save(WSIpred,file = fname)
   }
-  #save(x,file = paste0(save_folder,"x",j,".RData"))
-  TrainTest = TrainTestProducer(x,y)
-  save(TrainTest,file = paste0(save_folder,"split"))
 }
 
-
+#####################################################
