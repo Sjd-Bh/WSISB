5060f7a2cb0f76e22863d5ddcd5a0be781eb997d
Saba Bh <s.bahonar.bio@gmail.com>
2020-11-09 23:47:07 +0330

    n

---
+++ WSI/folder.R
@@ -0,0 +1,6 @@
+save_folder = "/home/montazeri/results/data/"
+saveFolder = "/home/montazeri/results/lassoModel/"
+
+
+dir.create(save_folder, recursive = TRUE, showWarnings = FALSE)
+dir.create(saveFolder, recursive = TRUE, showWarnings = FALSE)
